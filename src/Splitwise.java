02c48640733abaade5ed0c6a5f48e31c2361da49
Shivadarrshni <shivadarrshni18@gmail.com>
2026-07-28 18:45:51 +0530

    updated 1.6

--- src/Splitwise.java
+++ src/Splitwise.java
@@ -8,17 +8,37 @@ public class  Splitwise{
         System.out.println("Ready.More features coming in the next lesson");
 
         Scanner input =new Scanner(System.in);
+        boolean running = true;
+        while(running) {
+            System.out.println("===Menu===");
+            System.out.println("1.Record expense");
+            System.out.println("0.Quit");
+            System.out.print("Choice:");
+            int choice = input.nextInt();
+            input.nextLine();
 
+            switch (choice) {
 
-        System.out.print("Who paid? ");
-        String payerName = input.nextLine();
-        System.out.print("Total amount (₹)? ");
-        double totalAmount = input.nextDouble();
-        int numFriends=3;
-        double perPersonShare = totalAmount / numFriends;
-        String expenseLine = "%s paid ₹%.2f".formatted(payerName, totalAmount);
-        String shareLine = "Each person pays: %.2f".formatted(perPersonShare);
-        System.out.println(expenseLine);
-        System.out.println(shareLine);
+                case 1 -> {
+                    System.out.print("Who paid? ");
+                    String payerName = input.nextLine();
+                    System.out.print("Total amount (₹)? ");
+                    double totalAmount = input.nextDouble();
+                    int numFriends = 3;
+                    double perPersonShare = totalAmount / numFriends;
+                    String expenseLine = "%s paid ₹%.2f".formatted(payerName, totalAmount);
+                    String shareLine = "Each person pays: %.2f".formatted(perPersonShare);
+                    System.out.println(expenseLine);
+                    System.out.println(shareLine);
+                }
+
+                case 0 -> {
+                    System.out.println("Goodbye!!");
+                       running = false;
+                }
+
+                default -> System.out.println("Invalid choice.Try again");
+            }
+        }
     }
 }
