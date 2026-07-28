1dc9037b482acfa946731b9851636d4ac826424e
Shivadarrshni <shivadarrshni18@gmail.com>
2026-07-28 11:02:34 +0530

    updated 1.5

--- src/Splitwise.java
+++ src/Splitwise.java
@@ -8,9 +8,9 @@ public class  Splitwise{
         double totalAmount=500.0;
         int numFriends=3;
         double perPersonShare = totalAmount / numFriends;
-        System.out.println(payerName );
-        System.out.println(totalAmount);
-        System.out.println(numFriends);
-        System.out.println(perPersonShare);
+        String expenseLine = payerName + " paid ₹" + totalAmount;
+        String shareLine = "Each person pays: " + perPersonShare;
+        System.out.println(expenseLine);
+        System.out.println(shareLine);
     }
 }
